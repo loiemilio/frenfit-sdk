4e27cf782a82a044f279655f19bf450bfdc584dd
Emilio Loi <loi.emilio@gmail.com>
2023-07-02 14:17:03 +0000

    feat: add support for hiding entries

--- src/feed/reactions.ts
+++ src/feed/reactions.ts
@@ -1,7 +1,27 @@
 import { ffetch } from '@support/client';
+import { UnexpectedResponseException } from '@support/exceptions';
 import { evaluateReactionResponse } from '@support/reactions';
 
 import endpoints from './endpoints';
+
+import { getEntry } from '.';
+
+export const hideEntry = async (entryId: number) => {
+  const body = new URLSearchParams();
+  body.set('id', String(entryId));
+
+  const hideResponse = await ffetch(endpoints.hideEntry, {
+    method: 'POST',
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+    },
+    body,
+  });
+
+  const text = await hideResponse.text();
+
+  return text.includes(`restoreEntry('${entryId}')`);
+};
 
 export const toggleEntryDislike = async (entryId: number) => {
   const dislikeResponse = await ffetch(endpoints.dislikeEntry(entryId), {
@@ -23,4 +43,21 @@ export const toggleEntryLike = async (entryId: number) => {
   const text = await likeResponse.text();
 
   return evaluateReactionResponse(text);
+};
+
+export const unhideEntry = async (entryId: number) => {
+  const unhideResponse = await ffetch(endpoints.unhideEntry(entryId), {
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+    },
+  });
+
+  const text = await unhideResponse.text();
+  if (!text.includes(`id="${entryId}"`)) {
+    throw new UnexpectedResponseException(unhideResponse, {
+      missingEntry: 'The requested entry was not returned after un-hide',
+    });
+  }
+
+  return await getEntry(entryId);
 };
