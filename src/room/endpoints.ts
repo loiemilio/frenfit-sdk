96731667a7baa84a58b6e6a83347d6452ee79553
Emilio Loi <loi.emilio@gmail.com>
2023-07-02 18:32:30 +0000

    feat: add support for creating and editing rooms

--- src/room/endpoints.ts
+++ src/room/endpoints.ts
@@ -1,6 +1,8 @@
 import { frontendURL } from '@support/client';
 
 export default {
+  addRoom: frontendURL('room/save'),
+  editRoom: frontendURL('room/update'),
   myRooms: frontendURL('room/index'),
   toggleInHome: (id: number, inHome: 'true' | 'false') =>
     frontendURL('room/toggleHome?id={id}&inHome={inHome}', { id, inHome }),
